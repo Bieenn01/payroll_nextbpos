798d88a2a2eb8ce8cde70300c96d6c8cb4b7a3b2
John Doe <johndoe@email.com>
2024-02-26 08:39:16 +0800

    update

--- lib/backend/jsonfiles/add_records.dart
+++ lib/backend/jsonfiles/add_records.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -30,36 +30,3 @@ Future<void> addRecords(
     print('Error adding records: $e');
   }
 }
-=======
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-Future<void> addRecords(
-  String timeIn,
-  String timeOut,
-  String role,
-  String department,
-) async {
-  try {
-    final userId = FirebaseAuth.instance.currentUser!.uid;
-    final userDoc =
-        await FirebaseFirestore.instance.collection('User').doc(userId).get();
-    final userData = userDoc.data() as Map<String, dynamic>;
-
-    final json = {
-      'department': department,
-      'role': role,
-      'timeIn': timeIn,
-      'timeOut': timeOut,
-      'id': userDoc.id,
-      'userId': userId,
-      'userName':
-          '${userData['fname']} ${userData['mname']} ${userData['lname']}',
-    };
-
-    await FirebaseFirestore.instance.collection('Records').doc().set(json);
-  } catch (e) {
-    print('Error adding records: $e');
-  }
-}
->>>>>>> d44b4ba219fba472f5c52d80e97a4497e50679fd
--- lib/backend/jsonfiles/add_users.dart
+++ lib/backend/jsonfiles/add_users.dart
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -33,37 +33,3 @@ Future<void> addUser(
 
   await docUser.set(json);
 }
-=======
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
-Future<void> addUser(
-  fname,
-  mname,
-  lname,
-  email,
-  start_shift,
-  end_shift,
-  role,
-  department,
-  type_employee,
-) async {
-  final docUser = FirebaseFirestore.instance
-      .collection('User')
-      .doc(FirebaseAuth.instance.currentUser!.uid);
-
-  final json = {
-    'fname': fname,
-    'mname': mname,
-    'lname': lname,
-    'email': email,
-    'start_shift': start_shift,
-    'end_shift': end_shift,
-    'role': role,
-    'department': department,
-    'type_employee': type_employee,
-  };
-
-  await docUser.set(json);
-}
->>>>>>> d44b4ba219fba472f5c52d80e97a4497e50679fd
