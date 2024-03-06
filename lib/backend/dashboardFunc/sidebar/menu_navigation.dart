a12ac2523401f3d309d18007d8e6550fbf77a37e
kizygel <caezzygayle23@gmail.com>
2024-03-06 13:04:29 +0800

    init

--- lib/backend/dashboardFunc/sidebar/menu_navigation.dart
+++ lib/backend/dashboardFunc/sidebar/menu_navigation.dart
@@ -8,7 +8,6 @@ import 'package:project_payroll_nextbpo/backend/jsonfiles/regularOT.dart';
 import 'package:project_payroll_nextbpo/backend/jsonfiles/restDayOT.dart';
 import 'package:project_payroll_nextbpo/backend/jsonfiles/specialHolidayOT.dart';
 import 'package:project_payroll_nextbpo/frontend/dashboard/attendace_page.dart';
-import 'package:project_payroll_nextbpo/frontend/dashboard/attendace_page2.dart';
 import 'package:project_payroll_nextbpo/frontend/dashboard/dashboard_page.dart';
 import 'package:project_payroll_nextbpo/frontend/dashboard/pov_user_create.dart';
 import 'package:project_payroll_nextbpo/frontend/holiday/holiday.dart';
@@ -164,7 +163,7 @@ class ScreensView extends StatelessWidget {
               ),
               Flexible(
                 flex: 7,
-                child: AttendacePage(),
+                child: AttendancePage(),
               ),
             ],
           ),
