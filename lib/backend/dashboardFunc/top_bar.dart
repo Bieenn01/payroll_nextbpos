0d109994ff3d817bd3362e99505f20d0d28a611c
Bieenn01 <150262942+Bieenn01@users.noreply.github.com>
2024-03-21 15:43:25 +0800

    Update top_bar.dart

--- lib/backend/dashboardFunc/top_bar.dart
+++ lib/backend/dashboardFunc/top_bar.dart
@@ -219,17 +219,10 @@ class _TopBarState extends State<TopBar> {
                               const Icon(Icons.arrow_drop_down),
                             ],
                           )
-                        : CircleAvatar(
-                            backgroundImage: _role == 'Admin'
-                                ? const AssetImage('assets/images/Admin.jpg')
-                                : _role == 'Super Admin'
-                                    ? const AssetImage(
-                                        'assets/images/SAdmin.jpg')
-                                    : const AssetImage(
-                                        'assets/images/Employee.jpg'),
-                            // Change image path based on role
-                            radius:
-                                20, // Adjust the radius as per your requirement
+                        : const CircleAvatar(
+                            child: Icon(
+                              Icons.person,
+                            ),
                           ),
                   ),
                 ),
