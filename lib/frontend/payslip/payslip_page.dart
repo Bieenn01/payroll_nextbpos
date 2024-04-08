144560bb6991bf7765322217a4078b7fdfaa0c0c
dahndahn01 <tedlosdahnica@gmail.com>
2024-04-08 14:58:54 +0800

    init

--- lib/frontend/payslip/payslip_page.dart
+++ lib/frontend/payslip/payslip_page.dart
@@ -4,11 +4,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:project_payroll_nextbpo/frontend/contribution.dart';
+import 'package:project_payroll_nextbpo/frontend/payslip/contribution.dart';
 import 'package:shimmer/shimmer.dart' as ShimmerPackage;
 import 'package:intl/intl.dart';
 
-import 'package:project_payroll_nextbpo/frontend/payslip._form.dart';
+import 'package:project_payroll_nextbpo/frontend/payslip/payslip._form.dart';
 
 class PayslipData {
   final DateTime startDate;
