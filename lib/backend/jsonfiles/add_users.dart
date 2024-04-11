30b816539624158f737ac571e6db2ec1b381b217
John Doe <johndoe@email.com>
2024-04-11 09:28:30 +0800

    up

--- lib/backend/jsonfiles/add_users.dart
+++ lib/backend/jsonfiles/add_users.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> addUser(
+  // ignore: non_constant_identifier_names
   double monthly_salary, 
   String username,
   String fname,
